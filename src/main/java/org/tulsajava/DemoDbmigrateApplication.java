57dae519b2f86fc461024737750fa7fdb71e48ff
LeaderOne <markelba1@gmail.com>
2017-04-25 23:49:58 -0500

    Initial commit

---
+++ src/main/java/org/tulsajava/DemoDbmigrateApplication.java
@@ -0,0 +1,12 @@
+package org.tulsajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDbmigrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDbmigrateApplication.class, args);
+	}
+}
